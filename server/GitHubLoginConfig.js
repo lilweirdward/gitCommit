b8b654d2ac27555ad666537c7fdf8c8dcf0fb394
Albin George <albingeorge@ulsecure-belknap-205083.wireless.louisville.edu>
2014-04-22 22:48:20 -0400

    Front end stuff shows and works with Meteor and moved directories. Removed compass and bourbon stuff. Abstracted navigation html code to a navigation template under navigation.html

---
+++ server/GitHubLoginConfig.js
@@ -0,0 +1,11 @@
+ServiceConfiguration.configurations.remove({
+  service: "github"
+});
+
+
+//GitHub Test API Settings!
+ServiceConfiguration.configurations.insert({
+  service: "github",
+  clientId: "fc1a704150a8534b76f2",
+  secret: "64ceedc4586eab8b9e50b834fe3a5ddf78b6f513"
+});
